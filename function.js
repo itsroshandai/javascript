e5e4f00187eb5e5cf7dc83308f107f14133c9429
Roshan Phoju <roshanphoju@gmail.com>
2026-03-30 12:32:31 +0545

    learned loop and function

---
+++ function.js
@@ -0,0 +1,58 @@
+// function
+// function is block of code that perform specific task
+// function functionName(){
+//  logic
+// }
+// call
+// functionName()
+
+// function sum(a,b,c){
+//     return a+b+c
+// }
+// x=sum(1,2,3)
+// console.log(x)
+
+// write a function that will identify the given number is odd or even
+// function oddoreven(x){
+//     if(x%2==0){
+//         console.log(`${x} is even.`)
+//     }
+//     else{
+//         console.log(x+" is odd")
+//     }
+// }
+// oddoreven(8)
+// // write a function to count total even number between 1-20
+// function counteven(){
+//     let count=0;
+//     for(i=1;i<=20;i++){
+//         if(i%2==0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// count=counteven();
+// console.log("There are toatl "+count+" even numbers.")
+
+// types of function
+// 1. Arrow function
+// function sum(){
+// console.log("sum")
+// }
+// const sum=(a,b)=>{
+//     console.log(a)
+//     console.log(b)
+// }
+// const sum=(a,b)=> console.log(a+b);
+// sum(2,3)
+// const sum = a => console.log(a);
+// sum(1)
+// const sum=(a,b)=> a+b;
+// console.log(sum(1,2));
+
+// allows hoist in normal function only
+// printinfo()
+// function printinfo(){
+//     console.log("horw")
+// }
