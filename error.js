8096d555dbf12a89e5237a1ae7963bba8d43317e
Roshan Phoju <roshanphoju@gmail.com>
2026-04-08 19:45:06 -1000

    learnt error

---
+++ error.js
@@ -0,0 +1,35 @@
+// error handling
+// async- await / promise, fetch api
+// error handling or exceptional handling
+// fetch
+
+// try-catch
+
+// try{
+//     // logic or ricky code
+// }catch(error){
+//     console.log(error)
+// }
+
+
+// try{
+//     console.log(x)
+// }catch(error){
+//     console.log(error.name)
+//     console.log(error.message)
+//     console.log(error.stack)
+// }
+// console.log(100)
+
+// types of error
+// 1. Reference error       //while accessing the undefinde variables
+// 2. syntaxerror           
+// 3. typeError         //let x=10; x.toUpperCase();
+
+try{
+    console.log(x)
+}catch(error){
+    console.log(error.message)
+}finally{
+    console.log(10)
+}
